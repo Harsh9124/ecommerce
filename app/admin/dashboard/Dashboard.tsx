70c3fc41c786732aaf411de0f57a7d32a20946d9
Harsh <hmgundaliya@gmail.com>
2024-05-30 14:46:05 +0530

    ui changes

--- app/admin/dashboard/Dashboard.tsx
+++ app/admin/dashboard/Dashboard.tsx
@@ -19,42 +19,51 @@ const Dashboard = () => {
   if (!summary) return <div>Loading...</div>;
 
   return (
-    <div className="p-4 bg-light-grey">
-      <div className="my-4 stats inline-grid md:flex shadow stats-vertical md:stats-horizontal">
-        <div className="stat bg-white p-4 rounded-lg shadow-md m-2">
-          <div className="stat-title text-dark-grey">Sales</div>
-          <div className="stat-value text-primary">
-            ${formatNumber(summary.ordersPrice)}
+    <div className="p-4 my-5 bg-primary rounded-lg">
+      <h1 className="py-4 text-2xl font-semibold">DashBoard</h1>
+      <div className="flex flex-row">
+        <div className="stat bg-white p-4 rounded-lg shadow-md m-2 w-1/2">
+          <div className="stat-title font-semibold text-xl">Sales</div>
+          <div className="stat-value text-lime-500 my-2 text-lg ">
+            ₹{formatNumber(summary.ordersPrice)}
           </div>
           <div className="stat-desc">
-            <Link href="/admin/orders" className="link-details">
+            <Link href="/admin/orders" className="text-blue-600">
               View sales
             </Link>
           </div>
         </div>
-        <div className="stat bg-white p-4 rounded-lg shadow-md m-2">
-          <div className="stat-title text-dark-grey">Orders</div>
-          <div className="stat-value text-primary">{summary.ordersCount}</div>
+        <div className="stat bg-white p-4 rounded-lg shadow-md m-2 w-1/2">
+          <div className="stat-title font-semibold text-xl">Orders Placed</div>
+          <div className="stat-value  text-lime-500 my-2 text-lg">
+            {summary.ordersCount}
+          </div>
           <div className="stat-desc">
-            <Link href="/admin/orders" className="link-details">
+            <Link href="/admin/orders" className="text-blue-600">
               View orders
             </Link>
           </div>
         </div>
-        <div className="stat bg-white p-4 rounded-lg shadow-md m-2">
-          <div className="stat-title text-dark-grey">Products</div>
-          <div className="stat-value text-primary">{summary.productsCount}</div>
+      </div>
+      <div className="flex flex-row">
+        <div className="stat bg-white p-4 rounded-lg shadow-md m-2 w-1/2">
+          <div className="stat-title font-semibold text-xl">Products</div>
+          <div className="stat-value text-lime-500 my-2 text-lg">
+            {summary.productsCount}
+          </div>
           <div className="stat-desc">
-            <Link href="/admin/products" className="link-details">
+            <Link href="/admin/products" className="text-blue-600">
               View products
             </Link>
           </div>
         </div>
-        <div className="stat bg-white p-4 rounded-lg shadow-md m-2">
-          <div className="stat-title text-dark-grey">Users</div>
-          <div className="stat-value text-primary">{summary.usersCount}</div>
+        <div className="stat bg-white p-4 rounded-lg shadow-md m-2 w-1/2">
+          <div className="stat-title font-semibold text-xl">Users</div>
+          <div className="stat-value text-lime-500 my-2 text-lg">
+            {summary.usersCount}
+          </div>
           <div className="stat-desc">
-            <Link href="/admin/users" className="link-details">
+            <Link href="/admin/users" className="text-blue-600">
               View users
             </Link>
           </div>
